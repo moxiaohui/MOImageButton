4c44f51a3b0df53536ddabd67add97149f2f78ac
moxiaohui <xhmo@mobvoi.com>
2018-12-04 18:36:02 +0800

    按钮图片的4个位置

---
+++ MOImageButtonDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MOImageButtonDemo
+//
+//  Created by 莫晓卉 on 2018/12/4.
+//  Copyright © 2018年 moxiaohui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
